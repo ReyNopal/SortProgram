50b43d4bf4bf1edaac37d17ab42d4d4d1e05df36
Mayelo <mayelo@Mayelos-MacBook-Pro.local>
2021-03-18 00:33:08 -0400

    First Commit

---
+++ SortProgram/README.txt
@@ -0,0 +1,4 @@
+###############__README__##########
+# To run this code you need Python.
+# Navigate with the terminal, find SortPrograms/SortMe.py
+# Run the program and see the results. 
---
+++ SortProgram/SortMe.py
@@ -0,0 +1,38 @@
+def readFile():
+	
+	studentList = list()
+	#read the file 
+	readFromFile = open("SortMe.txt", "r")
+	
+	#append each name to studentList
+	for eachName in readFromFile:
+		studentList.append(eachName)
+		#print(eachName)
+		
+	readFromFile.close()
+	
+	sortListNames(studentList)	
+	
+	
+def sortListNames(studentList):
+	studentList.sort()
+	studentList.sort(key=len)
+	
+	###########################
+	#for testing purposes only 
+	printList(studentList)
+	###########################
+	
+	return studentList
+	
+#############################	
+def printList(thisList):
+	for item in thisList:
+		print(item)
+#############################	
+	
+if __name__ == '__main__':
+	readFile()
+	
+	
+	
